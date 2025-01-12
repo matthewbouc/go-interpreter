9d89466d6de526fb014720fd024d6bcba850c623
Matthew Bouc <boucgm@gmail.com>
2025-01-11 19:55:12 -0600

    lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,1 +1,43 @@
 package lexer
+
+import (
+	"go/token"
+
+	"github.com/matthewbouc/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int  // current position
+	readPosition int  // next position / read
+	ch           byte // current char being examined
+	// ** the ch could be a `rune` and support unicode (and emojis).. for further fun.
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	}
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
